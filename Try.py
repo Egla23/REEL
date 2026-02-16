90d67e9be4c412eeb706d44ac61bb9e36a39e5d8
Egla <eglatesfu@gmail.com>
2026-02-16 18:42:37 -0500

    caling functions / practice

---
+++ Try.py
@@ -0,0 +1,14 @@
+from utils import get_chroma_client, get_or_create_collection,query_collection,format_results_as_context
+
+def main():
+    print("hello")
+    # call functions here
+    client = get_chroma_client("./chroma_db")
+    collection = get_or_create_collection(client, "docs")
+    results = query_collection(collection, "dj", n_results=3)
+    contexts= format_results_as_context(results) # 
+    print(contexts)
+if __name__ == "__main__":
+    main() # will run main when you click run
+
+
