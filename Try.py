debd129eaafa4aac4f4dc8cc9eefe64551e88abc
soch04 <tempo.hardware999@slmails.com>
2026-02-19 17:06:34 -0500

    added input loop

--- Try.py
+++ Try.py
@@ -5,9 +5,15 @@ def main():
     # call functions here
     client = get_chroma_client("./chroma_db")
     collection = get_or_create_collection(client, "docs")
-    results = query_collection(collection, "dj", n_results=3)
-    contexts= format_results_as_context(results) # 
-    print(contexts)
+
+    i = input()
+    while i != 'q': # loops indefinatley until input is 'q'
+        results = query_collection(collection, i, n_results=3)
+        contexts= format_results_as_context(results) 
+        print(contexts)
+        i = input()
+    
+    print('ended')
 if __name__ == "__main__":
     main() # will run main when you click run
 
